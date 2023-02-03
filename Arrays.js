d2dea3df854dc2001a6a80bb0b185e0ab6d42ace
IrynaDesigna <irynadesigna@gmail.com>
2023-02-02 23:20:30 -0700

    feat: arrays compact solution

--- Arrays.js
+++ Arrays.js
@@ -1,4 +1,5 @@
-export const chunk = (arr, len) => {
+// Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+const chunk = (arr, len) => {
   let result = [];
   let chunk = [];
   let chunkIndex = 0;
@@ -14,4 +15,31 @@ export const chunk = (arr, len) => {
     }
   }
   return result;
+}
+
+// Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+const compact = (arr) => {
+  const falsey = [false, null, 0, '', undefined, NaN];
+  let result = [];
+  let resultIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    let isFalsey = false;
+    for (let j = 0; j < falsey.length; j++) {
+      if (item === falsey[j]) {
+        isFalsey = true;
+        break;
+      }
+    }
+    if (!isFalsey) {
+      result[resultIndex] = item;
+      resultIndex++;
+    }
+  }
+  return result;
+}
+
+export const objectArr = {
+  chunk: chunk,
+  compact: compact
 }
--- Arrays.test.js
+++ Arrays.test.js
@@ -1,21 +1,24 @@
-import {chunk} from './Arrays';
+import {objectArr} from './Arrays';
 
+// chunk testing
 test('chunk function exists', () => {
-    expect(chunk).toBeDefined();
+    expect(objectArr.chunk).toBeDefined();
 });
 
 test('Chunk the array of 10 values with length of 2', () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const len = 2;
-    const chunkedArr = chunk(numbers, len);
-
+    const chunkedArr = objectArr.chunk(numbers, len);
+    
     expect(chunkedArr).toEqual([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]);
 });
 
-test('Chunk an array of 3 values with length of 1', () => {
-    const numbers = [1, 2, 3];
-    const len = 1;
-    const chunkedArr = chunk(numbers, len);
-  
-    expect(chunkedArr).toEqual([[1], [2], [3]]);
+
+// compact testing
+test('compact function exists', () => {
+    expect(objectArr.compact).toBeDefined();
+});
+
+test('Compact an array of [0, 1, false, 2, "", 3] to [1, 2, 3]', () => {
+    expect(objectArr.compact([0, 1, false, 2, "", 3])).toEqual([1, 2, 3]);
 });
