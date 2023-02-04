3188c2f30078345f175d8a69c5120f13c9614c25
IrynaDesigna <irynadesigna@gmail.com>
2023-02-04 12:34:06 -0700

    feat: object omit function solution

--- Objects.js
+++ Objects.js
@@ -2,7 +2,7 @@
 //         Source properties that resolve to undefined are skipped if a destination value exists. 
 //         Array and plain object properties are merged recursively. Other objects and value types are overridden by assignment.
 //         Source objects are applied from left to right. Subsequent sources overwrite property assignments of previous sources.
-function merge(obj, ...sources) {
+const merge = (obj, ...sources) => {
     for (const source of sources) {
       for (const key of Object.keys(source)) {
         if (obj[key] && typeof obj[key] === 'object') {
@@ -20,8 +20,34 @@ function merge(obj, ...sources) {
     }
     return obj;
 }
+
+// Omit - this method creates an object composed of the own and inherited enumerable property paths of object that are not omitted.
+const omit = (obj, strings) => {
+    let result = {};
+    const keys = Object.keys(obj);
+  
+    if (!Array.isArray(strings)) {
+      strings = [strings];
+    }
+  
+    for (let i = 0; i < keys.length; i++) {
+      let omitKey = false;
+      for (let j = 0; j < strings.length; j++) {
+        if (keys[i] === strings[j]) {
+          omitKey = true;
+          break;
+        }
+      }
+      if (!omitKey) {
+        result[keys[i]] = obj[keys[i]];
+      }
+    }
+  
+    return result;
+  };
   
 // funcObject with functions
 export const funcObject = {
-    merge: merge
+    merge: merge,
+    omit: omit
 }
