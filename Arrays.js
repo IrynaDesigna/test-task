50484da94436a807edee69c11cd20ef1acebf23a
IrynaDesigna <irynadesigna@gmail.com>
2023-02-03 21:06:13 -0700

    feat: take function solution

--- Arrays.js
+++ Arrays.js
@@ -1,4 +1,4 @@
-// Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+// Chunck - creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
 const chunk = (arr, len) => {
   let result = [];
   let chunk = [];
@@ -17,7 +17,7 @@ const chunk = (arr, len) => {
   return result;
 }
 
-// Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+// Compact - creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
 const compact = (arr) => {
   const falsey = [false, null, 0, '', undefined, NaN];
   let result = [];
@@ -39,7 +39,7 @@ const compact = (arr) => {
   return result;
 }
 
-// Drop creates a slice of array with n elements dropped from the beginning
+// Drop - creates a slice of array with n elements dropped from the beginning
 const drop = (arr, n=1) => {
   let result = [];
   let resultLength = arr.length - n;
@@ -54,9 +54,22 @@ const drop = (arr, n=1) => {
   return result;
 }
 
+//  Take - creates a slice of array with n elements taken from the beginning
+const take = (arr, n=1) => {
+  let result = [];
+  if (n === 1) return result[0] = arr[0];
+  if (n === 0) return result;
+  if (n >= arr.length) return arr;
+  for (let i = 0; i < n; i++) {
+    result[i] = arr[i];
+  }
+  return result;
+}
+
 // object Array with functions
 export const objectArr = {
   chunk: chunk,
   compact: compact,
-  drop: drop
+  drop: drop,
+  take: take
 }
